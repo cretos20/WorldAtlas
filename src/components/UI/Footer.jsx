0b1d6ad8f68be87800e55e07a94eb3b2bc572594
cretos20 <nityaraval011@gmail.com>
2025-11-20 12:20:45 +0530

    ui modifications

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,5 +1,4 @@
 import footerContact from "../../api/footerApi.json";
-import { IoCallSharp } from "react-icons/io5";
 import { MdPlace } from "react-icons/md";
 import { TbMailPlus } from "react-icons/tb";
 
@@ -8,7 +7,6 @@ import { NavLink } from "react-router-dom";
 export const Footers = () => {
   const footerIcon = {
     MdPlace: <MdPlace />,
-    IoCallSharp: <IoCallSharp />,
     TbMailPlus: <TbMailPlus />,
   };
 
@@ -27,49 +25,6 @@ export const Footers = () => {
             </div>
           );
         })}
-      </div>
-
-      <div className="copyright-area">
-        <div className="container">
-          <div className="grid grid-two-cols">
-            <div className="copyright-text">
-              <p>
-                Copyright &copy; 2024, All Right Reserved
-                <NavLink to="https://thapatechnical.shop/" target="_blank">
-                  ThapaTechnical
-                </NavLink>
-              </p>
-            </div>
-
-            <div className="footer-menu">
-              <ul>
-                <li>
-                  <NavLink to="/">Home</NavLink>
-                </li>
-
-                <li>
-                  <NavLink
-                    to="https://www.instagram.com/thapatechnical/"
-                    target="_blank"
-                  >
-                    Social
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink
-                    to="https://thapatechnical.shop/source-code"
-                    target="_blank"
-                  >
-                    Source Code
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink to="/contact">Contact</NavLink>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
       </div>
     </footer>
   );
